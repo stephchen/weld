abe743bbf0e132d6400847e49a211426dd8d7cd7
stephanie chen <stephanie751@gmail.com>
2018-12-09 15:38:52 -0800

    use squared dists

--- python/sklearn/weldsklearn/nearest_neighbors.py
+++ python/sklearn/weldsklearn/nearest_neighbors.py
@@ -21,8 +21,6 @@ class WeldKNeighbors(object):
         # single sample
         weldobj = WeldObject(NumPyEncoder(), NumPyDecoder())
 
-
-        
         template = """
             let idx = 
             @(loopsize: %(xlen)sL)
@@ -35,7 +33,6 @@ class WeldKNeighbors(object):
                     ));
                     let mindist = select(dist < p.$2, dist, p.$2);
                     let minidx = select(dist < p.$2, p.$1, p.$3);
-
                     { {
                         p.$0, p.$1 + 1L, 
                         mindist, minidx
@@ -50,22 +47,62 @@ class WeldKNeighbors(object):
             'xtrain': weldobj.update(self.xtrain, WeldVec(WeldVec(WeldFloat()))),
             'ytrain': weldobj.update(self.ytrain, WeldVec(WeldFloat())),
             'x': weldobj.update(x, WeldVec(WeldFloat())),
-            'xlen': str(len(self.xtrain)),
-            'distinit': weldobj.update(distinit, WeldVec(WeldFloat()))
+            'xlen': str(len(self.xtrain))
         }
         res = weldobj.evaluate(WeldFloat())
         # res = weldobj.evaluate(WeldFloat())
 
         return res
 
+
     def score(self, x, y):
-        score = 0
-        for i, xt in enumerate(x):
-            pred = self.predict(xt)
-            print i, pred, y[i]
-            if pred == y[i]:
-                score += 1
-        return float(score) / len(x)
+        template = """
+            @(loopsize: %(testlen)sL)
+            result(for(
+                zip(%(x)s, %(y)s),
+                merger[f32, +],
+                |b, i, e| let idx = 
+                    @(loopsize: %(xlen)sL)
+                    iterate(
+                        { %(xtrain)s, 0L, f32(1000000000), 0L},
+                        |p|
+                            let xt = lookup(p.$0, p.$1);
+                            let dist = result(
+                                for(zip(e.$0, xt), merger[f32, +], |b2, i2, e2| merge(b2, (e2.$0 - e2.$1) * (e2.$0 - e2.$1)))
+                            );
+                            let mindist = select(dist < p.$2, dist, p.$2);
+                            let minidx = select(dist < p.$2, p.$1, p.$3);
+                            { {
+                                p.$0, p.$1 + 1L, 
+                                mindist, minidx
+                            }, p.$1 < %(xlen)sL - 1L}).$3;
+                if(lookup(%(ytrain)s, idx) == e.$1, merge(b, f32(1)), merge(b, f32(0)))
+            ))
+        """
+
+        weldobj = WeldObject(NumPyEncoder(), NumPyDecoder())
+        weldobj.weld_code = template % {
+            'xtrain': weldobj.update(self.xtrain, WeldVec(WeldVec(WeldFloat()))), # 0
+            'ytrain': weldobj.update(self.ytrain, WeldVec(WeldFloat())), # 1
+            'x': weldobj.update(x, WeldVec(WeldVec(WeldFloat()))), # 2
+            'y': weldobj.update(y, WeldVec(WeldFloat())), # 3
+            'xlen': str(len(self.xtrain)),
+            'testlen': str(len(x))
+        }
+
+        score = weldobj.evaluate(WeldFloat())
+
+        return score / len(x)
+
+
+    # def score(self, x, y):
+    #     score = 0
+    #     for i, xt in enumerate(x):
+    #         pred = self.predict(xt)
+    #         print i, pred, y[i]
+    #         if pred == y[i]:
+    #             score += 1
+    #     return float(score) / len(x)
 
 
 
